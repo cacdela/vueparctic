683b6b4923559134b166c9e29f595c6915bdf83b
cacdela <vladimircacdela@gmail.com>
2024-01-16 21:52:08 +0700

    Практическая работа №4

--- vue/js/main.js
+++ vue/js/main.js
@@ -12,6 +12,19 @@ let app = new Vue({
         onSale: true,
         description,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-     }     
- }) 
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
+
+    }
+ })
+
