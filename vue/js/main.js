f42f123852cedc3ad63d777990bc26ad2082e086
cacdela <vladimircacdele@gmail.com>
2024-01-17 13:15:13 +0700

    Вычисляемые свойства

--- vue/js/main.js
+++ vue/js/main.js
@@ -5,9 +5,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product,
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: false,
         inventory: 120,
         onSale: true,
         description,
@@ -20,17 +20,15 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-    },
-    beforeUpdate() {
-        this.inStock = this.inventory > 0 ? true : false;
-
     },
     methods: {
         addToCart() {
@@ -39,10 +37,24 @@ let app = new Vue({
         removToCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         },
     }
 
 })
+
+
 
