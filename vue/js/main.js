e8bf006da8cce8f17fb276212da69cae2a2cb2fc
cacdela <vladimircacdela@gmail.com>
2024-01-17 23:56:15 +0700

    Пользовательские события

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,5 +1,5 @@
 let product = "Socks";
-let description = "A pair of warm, fuzzy socks."
+let description = "A pair of warm, fuzzy socks.";
 
 Vue.component('product-details', {
     props: {
@@ -17,74 +17,57 @@ Vue.component('product-details', {
     `
 });
 
-
 Vue.component('product', {
     template: `
-   <div class="product">
-
-        <div class="product-image">
-            <img v-bind:src="image" v-bind:alt="altText" />
-        </div>
-
-        <div class="product-info">
-            <h1>{{ title }}</h1>
-            <p>{{ sale }}</p>
-            <p>Shipping: {{ shipping }}</p>
-
-            <div class="product-quantity">
-                <p v-if="inStock" class="inStock">In stock</p>
-                <p v-else class="outOfStock">Out of Stock</p>
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" :alt="altText" />
             </div>
 
-            <div
-                    class="color-box"
-                    v-for="(variant, index) in variants"
-                    :key="variant.variantId"
-                    :style="{ backgroundColor:variant.variantColor }"
-                    @mouseover="updateProduct(index)"
-            ></div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p>{{ sale }}</p>
+                <p>Shipping: {{ shipping }}</p>
 
-            <product-details :details="details"></product-details>
+                <div class="product-quantity">
+                    <p v-if="inStock" class="inStock">In stock</p>
+                    <p v-else class="outOfStock">Out of Stock</p>
+                </div>
 
-            <p>Size</p>
-            <ul>
-                <li v-for="size in sizes">{{ size }}</li>
-            </ul>
+                <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
+                     :style="{ backgroundColor: variant.variantColor }" @mouseover="updateProduct(index)"></div>
 
-            <div v-for="variant in variants" :key="variant.variantId">
-                <p>{{ variant.variantColor }}</p>
-            </div>
+                <product-details :details="details"></product-details>
 
-            <div class="cart">
-                <p>Cart({{ cart }})</p>
-                <button
-                        v-on:click="addToCart"
-                        :disabled="!inStock"
-                        :class="{ disabledButton: !inStock }"
-                >
+                <p>Size</p>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
+
+                <div v-for="variant in variants" :key="variant.variantId">
+                    <p>{{ variant.variantColor }}</p>
+                </div>
+
+                <div class="product-name">
+                    <p>{{ description }}</p>
+                </div>
+
+                <div class="product-a">
+                    <a :href="link">More products like this</a>
+                </div>
+
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
                     Add to cart
                 </button>
-                <button v-on:click="removToCart">Remov to cart</button>
             </div>
-
-            <div class="product-name">
-                <p>{{ description }}</p>
-            </div>
-
-            <div class="product-a">
-                <a :href="link">More products like this</a>
-            </div>
-
         </div>
-    </div>
- `,
+    `,
     props: {
         premium: {
             type: Boolean,
             required: true
         },
     },
-
     data() {
         return {
             product,
@@ -94,7 +77,6 @@ Vue.component('product', {
             inventory: 120,
             onSale: true,
             description,
-            cart: 0,
             link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -112,19 +94,18 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
-        }
+        };
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
-        removToCart() {
-            this.cart -= 1
+        removeFromCart() {
+            this.$emit('remove-from-cart');
         },
         updateProduct(index) {
             this.selectedVariant = index;
-            console.log(index);
-        }
+        },
     },
     computed: {
         title() {
@@ -133,8 +114,8 @@ Vue.component('product', {
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         },
         sale() {
             if (this.onSale) {
@@ -144,20 +125,20 @@ Vue.component('product', {
             }
         },
         shipping() {
-            if (this.premium) {
-                return "Free";
-            } else {
-                return 2.99
-            }
+            return this.premium ? "Free" : 2.99;
         },
-    }
-})
+    },
+});
+
 let app = new Vue({
     el: '#app',
     data: {
-        premium: true
-    }
-})
-
-
-
+        premium: true,
+        cart: [],
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+    },
+});
